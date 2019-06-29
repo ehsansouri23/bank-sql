3b9da1529f6978e32dbcc86b911d02e373ebf3c5
Ehsan <ehsansouri23@gmail.com>
2019-06-29 16:24:49 +0430

    add phone number and address and settings and paymant tables

--- tables.sql
+++ tables.sql
@@ -6,6 +6,18 @@ create table if not exists customer(
     c_melli int,
     c_pass varchar(20));
     
+create table if not exists call_number(
+	c_id int not null,
+    c_phone int,
+    primary key(c_id, c_phone),
+    foreign key(c_id) references customer(c_id));
+    
+create table if not exists address(
+	c_id int not null,
+    c_address varchar(50),
+    primary key(c_id, c_address),
+    foreign key(c_id) references customer(c_id));
+    
 create table if not exists bank_account(
 	a_id int not null primary key auto_increment,
     a_balance int,
@@ -39,5 +51,22 @@ create table if not exists account_owner(
     primary key(c_id, a_id),
     foreign key(c_id) references customer(c_id),
     foreign key(a_id) references bank_account(a_id)); 
+    
+create table if not exists settings(
+	c_id int not null,
+    a_id int not null,
+    s_name varchar(20),
+    s_color varchar(20),
+    primary key(c_id, a_id),
+    foreign key(c_id) references customer(c_id),
+    foreign key(a_id) references bank_account(a_id));
+    
+create table if not exists payment(
+	p_id int not null primary key auto_increment,
+    p_note varchar(50),
+    p_maker int not null,
+    a_id int not null,
+    foreign key(a_id) references bank_account(a_id));
+
     
     
